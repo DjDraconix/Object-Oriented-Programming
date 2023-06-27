3f58a440d452630a7d77fa2e67ef81fe56079500
DjDraconix <137447786+DjDraconix@users.noreply.github.com>
2023-06-27 14:48:50 -0600

    Update SavingsAccount.java

--- SavingsAccount.java
+++ SavingsAccount.java
@@ -1,4 +1,26 @@
 
-public class SavingsAccount {
+public class SavingsAccount extends Account {
+	double minimumBallance = 0.0;
 
+	SavingsAccount(int newId, double newBallance) {
+		super(newId, newBallance);
+	}
+	
+	public void withdrawFromSavigs(double amount) {
+		if (getBallance() - amount > 0) {
+			setBallance(getBallance() - amount);
+		} else {
+			System.out.println("This account dose not allow overdrafting."
+					+ "\nYou have " + getBallance() + " left in your account.");
+		}
+	}
+	
+	public void depositInSavings(double amount) {
+		setBallance(getBallance() + amount);
+	}
+	
+	public String toString() {
+		return "Acount Id: " + getId() + "\nDate Created: " + getDate() + 
+				"\nBallance: " + getBallance();
+	}
 }
