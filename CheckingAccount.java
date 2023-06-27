e5a27dbdcb4d784619b2c888145a712d54fc81fc
DjDraconix <137447786+DjDraconix@users.noreply.github.com>
2023-06-27 14:56:29 -0600

    Update CheckingAccount.java

--- CheckingAccount.java
+++ CheckingAccount.java
@@ -1,4 +1,19 @@
 
-public class CheckingAccount {
-
+public class CheckingAccount extends Account {
+	double overdraft = 50.0;
+	
+	CheckingAccount(int newId, double newBallance, double over) {
+		super(newId, newBallance);
+		overdraft = over;
+	}
+	
+	public void withdrawFromChecking(double amount) {
+		if (getBallance() - amount > 0) {
+			setBallance(getBallance() - amount);
+		} else if (Math.abs(getBallance() - amount) < overdraft) {
+			setBallance(getBallance()-amount);
+		} else {
+			System.out.println("Not enuff funds, overdraft is at " + overdraft);
+		}
+	}
 }
