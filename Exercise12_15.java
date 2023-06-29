06b39ae46794195c26173f9e3a5b9ab1e9fdacf1
DjDraconix <137447786+DjDraconix@users.noreply.github.com>
2023-06-29 09:57:23 -0600

    Update Exercise12_15.java

--- Exercise12_15.java
+++ Exercise12_15.java
@@ -7,7 +7,7 @@ public class Exercise12_15 {
 
 	public static void main(String[] args) throws IOException {
 		
-		File ex15 = new File("Exercise12_15.txt");
+		File ex15 = new File("Exercis15.txt");
 		ex15.createNewFile();
 		if (ex15.exists()) {
 			System.out.println("Is there");
@@ -16,24 +16,24 @@ public class Exercise12_15 {
 			System.exit(0);
 		}
 		
-		Scanner in = new Scanner(new File("Exercise12_15.txt"));
-		PrintWriter fout = new PrintWriter("Exercise12_15.txt");
+		Scanner in = new Scanner(ex15);
+		PrintWriter fout = new PrintWriter(ex15);
 		for (int i = 0; i < 100; i++) {
 			int num = (int) (Math.random() * 100);
 			System.out.print(num + " ");
-			fout.write(num + " ");
+			fout.print(num);
+			fout.print(" ");
 		}
 		System.out.println();
 		int counter = 0;
 		while (in.hasNext()) {
+			String num = in.next();
+			System.out.print(num + " ");
 			counter++;
 		}
 		System.out.println(counter);
-		
-		
 		
 		fout.close();
 		in.close();
 	}
 
-}
